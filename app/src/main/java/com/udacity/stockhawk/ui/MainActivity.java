31a7f2f0b126a0e71f5ffb514d620be7511e8c5e
oscarb <git@oscarb.se>
2017-03-16 23:09:28 +0100

    Add comments for localization

--- app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/MainActivity.java
@@ -161,8 +161,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         if (PrefUtils.getDisplayMode(this)
                 .equals(getString(R.string.pref_display_mode_absolute_key))) {
             item.setIcon(R.drawable.ic_percentage);
+            item.setTitle(getString(R.string.action_change_units, getString(R.string.unit_percentage)));
         } else {
             item.setIcon(R.drawable.ic_dollar);
+            item.setTitle(getString(R.string.action_change_units, getString(R.string.unit_absolute)));
         }
     }
 
